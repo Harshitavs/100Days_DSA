700f1be90d7b838c9204c51ebd8f2da52b5b66f1
Harshita Mathur <mail2harshitamathur@gmail.com>
2026-02-05 08:48:21 +0530

    Remove commented code and fix formatting

--- Day1Q2.c
+++ Day1Q2.c
@@ -1,36 +1,5 @@
-/**
- * Note: The returned array must be malloced, assume caller calls free().
- 
-#include<stdio.h>
-#include<stdlib.h>
 
-int main(){
-    int n , target, j ;
-    scanf("%d",&n);
-
-    int *nums = (int*)malloc(n * sizeof(int));
-    if(nums == NULL) {
-        printf("Memory allocation failed\n");
-        return -1;
-}
-for ( int i = 0 ; i<n;i++){
-    scanf("%d", (nums + i));
-}
-scanf("%d", &target);
-for (int i = 0 ; i<n;i++){
-    for (int j = i +1 ; j < n ; j++){
-    if ( *(nums+ i) + *(nums + j) == target) {
-        printf("%d %d", i, j);
-        free (nums);
-        return 0;
-    }
-}
-}
-
-free(nums);
-return 0;
-}
-*/
+// Note: The returned array must be malloced, assume caller calls free().
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -62,4 +31,5 @@ int main() {
 
     free(nums);
     return 0;
+
 }
